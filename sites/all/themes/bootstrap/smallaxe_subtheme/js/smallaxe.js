527d6233da9991a699383e79a7142faa29d42d4f
root <root@li493-99.members.linode.com>
2015-11-03 08:43:18 +0000

    first commit

---
+++ sites/all/themes/bootstrap/smallaxe_subtheme/js/smallaxe.js
@@ -0,0 +1,8 @@
+jQuery(document).ready(function(){
+
+//   var ofhtml = jQuery(".views-slideshow-controls-bottom").html();
+    jQuery(".views-slideshow-controls-text-previous").text("<");
+    jQuery(".views-slideshow-controls-text-next").text(">");
+//   jQuery("#views_slideshow_cycle_main_timeline_custom_view-block").append(ofhtml);
+//    jQuery("#views_slideshow_cycle_main_timeline_custom_view-block").on();
+});
